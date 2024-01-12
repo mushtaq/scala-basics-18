1bfd85a5561336022eca6712dc50429258956b0b
Mushtaq Ahmed <mushtaq@mushtaq-mac.local>
2024-01-12 18:55:30 +0530

    conc4

--- src/main/scala/conc/Account.scala
+++ src/main/scala/conc/Account.scala
@@ -6,13 +6,17 @@ import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 
 class Account {
 
-  private val executorService = Executors.newFixedThreadPool(1)
+  private val executorService = Executors.newSingleThreadExecutor(
+    Thread.ofVirtual().factory()
+  )
+
   private implicit val ec: ExecutionContext =
     ExecutionContext.fromExecutorService(executorService)
 
   private var balance = 0
 
   def deposit(amount: Int) = Future {
+//    println(Thread.currentThread())
     // atomic block
     // step1
     // step2
@@ -35,14 +39,16 @@ object Account {
   def main(args: Array[String]): Unit = {
     val account = new Account
 
-    val executorService = Executors.newFixedThreadPool(10)
+    val executorService = Executors.newVirtualThreadPerTaskExecutor()
     implicit val ec = ExecutionContext.fromExecutorService(executorService)
 
-    val results: Seq[Future[Future[Unit]]] = (1 to 100).map { x =>
+    val results: Seq[Future[Future[Unit]]] = (1 to 1000).map { x =>
       val future = account.deposit(1)
       Future(future)
     }
 
+
+    results.map(_.flatten.block())
     // Seq[Future[_]] => Future[Seq[_]]
     val future: Future[Seq[Unit]] = Future.sequence(results.map(_.flatten))
 
